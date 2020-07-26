08857502f901f6a314f812c08c83c0565a006d8e
SirPatagon <sirpatagon@gmail.com>
2020-07-26 22:12:10 +0200

    Add Post model

--- Puzzle.Data/Models/Blog.cs
+++ Puzzle.Data/Models/Blog.cs
@@ -6,7 +6,7 @@ using System.Text;
 namespace Puzzle.Data.Models
 {
     /// <summary>
-    /// 
+    /// A Group of Posts.
     /// </summary>
     [Table]
     public class Blog
@@ -20,8 +20,13 @@ namespace Puzzle.Data.Models
         /// <summary>
         /// The title of the <see cref="Blog"/> which will be used in the URL and frontend.
         /// </summary>
+        [Column]
         public string Title { get; set; }
 
-
+        /// <summary>
+        /// Pages this <see cref="Page"/> is the direct parent of.
+        /// </summary>
+        [Association(ThisKey = nameof(ID), OtherKey = nameof(Post.BlogID), CanBeNull = true, Relationship = Relationship.OneToMany)]
+        public List<Post> Posts { get; set; }
     }
 }
---
+++ Puzzle.Data/Models/Post.cs
@@ -0,0 +1,38 @@
+﻿using LinqToDB.Mapping;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Puzzle.Data.Models
+{
+    /// <summary>
+    /// A Post.
+    /// </summary>
+    [Table]
+    public class Post
+    {
+        /// <summary>
+        /// ID.
+        /// </summary>
+        [Column, PrimaryKey, Identity]
+        public Guid ID { get; set; }
+
+        /// <summary>
+        /// HTML content of the <see cref="Post"/>.
+        /// </summary>
+        [Column]
+        public string HTML { get; set; }
+
+        /// <summary>
+        /// Foreign key to <see cref="Blog"/>.
+        /// </summary>
+        [Column]
+        public Guid BlogID { get; set; }
+
+        /// <summary>
+        /// The parent <see cref="Blog"/>.
+        /// </summary>
+        [Association(ThisKey = nameof(BlogID), OtherKey = nameof(Blog.ID), CanBeNull = false, Relationship = Relationship.ManyToOne)]
+        public Blog ParentBlog { get; set; }
+    }
+}
