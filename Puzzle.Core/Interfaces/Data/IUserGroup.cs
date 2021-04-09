83c0964a7b713e8f1106e15659a14995bd5fa96d
SirPatagon <sirpatagon@gmail.com>
2021-04-09 22:52:59 +0200

    Deleted right management
    Authorization will be handled by another project now. Since it is technically optional, authorization will be a plugin

--- Puzzle.Core/Interfaces/Data/IUserGroup.cs
+++ Puzzle.Core/Interfaces/Data/IUserGroup.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Puzzle.Core.Enums;
+using System;
 using System.Collections.Generic;
 
 namespace Puzzle.Core.Interfaces.Data
@@ -17,10 +18,5 @@ namespace Puzzle.Core.Interfaces.Data
         /// List of <see cref="IUser"/>s this <see cref="IUserGroup"/> is the parent of.
         /// </summary>
         IEnumerable<IUser> Users { get; }
-
-        /// <summary>
-        /// List of Associations of <see cref="IRight"/>s for this UserGroup.
-        /// </summary>
-        IEnumerable<IUserGroupRight> UserGroupRights { get; }
     }
 }
--- Puzzle.Data/Models/UserGroup.cs
+++ Puzzle.Data/Models/UserGroup.cs
@@ -22,10 +22,5 @@ namespace Puzzle.Data.Models
         [Association(ThisKey = nameof(ID), OtherKey = nameof(User.UserGroupID), CanBeNull = true, Relationship = Relationship.OneToMany)]
         public IEnumerable<User> Users { get; set; }
         IEnumerable<IUser> IUserGroup.Users => throw new NotImplementedException();
-
-        /// <inheritdoc cref="IUserGroup.UserGroupRights"/>
-        [Association(ThisKey = nameof(ID), OtherKey = nameof(UserGroupRight.UserGroupID), CanBeNull = true, Relationship = Relationship.OneToMany)]
-        public IEnumerable<UserGroupRight> UserGroupRights { get; set; }
-        IEnumerable<IUserGroupRight> IUserGroup.UserGroupRights => throw new NotImplementedException();
     }
 }
